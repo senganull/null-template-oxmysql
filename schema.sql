db2e5db979b799428a2a1402892c931f0dc10489
null-laptop <170277151+senganull@users.noreply.github.com>
2026-02-10 15:43:23 +0900

    アポストロフィーからバッククォートに修正

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS 'null-template-users'(
-    'identifier' VARCHAR(60) NOT NULL,
-    'name' VARCHAR(50) DEFAULT 'Unknown',
-    'money' INT(12) DEFAULT 0,
-    'job' VARCHAR(20) DEFAULT 'unemployed',
+CREATE TABLE IF NOT EXISTS `null_template_users`(
+    `identifier` VARCHAR(60) NOT NULL,
+    `name` VARCHAR(50) DEFAULT 'Unknown',
+    `money` INT(12) DEFAULT 0,
+    `job` VARCHAR(20) DEFAULT 'unemployed',
 
-    PRIMARY KEY('identifier')
+    PRIMARY KEY(`identifier`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
