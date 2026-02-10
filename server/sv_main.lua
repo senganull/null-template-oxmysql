15267f9a0062c9ef7f5f95f303fb444b0c1b1cd3
null-laptop <170277151+senganull@users.noreply.github.com>
2026-02-10 15:33:52 +0900

    SyncPlayerToDatabase関数を実装

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -8,6 +8,29 @@ local function GetPlayerLicense(source)
     return nil
 end
 
+local function SyncPlayerToDatabase(source)
+    local identifier = GetPlayerLicense(source)
+    local playerName = GetPlayerName(source)
+
+    if not identifier then return end
+
+    --初期値としてのデータ(実際は変数から取得)
+    local defaultMoney = 5000
+    local defaultJob = "umemployed"
+
+    -- ON DUPLICATE KEY UPDATEを使うことで、既存なら更新
+    -- なければ新規作成を1回で行う
+    MySQL.update.await([[
+        INSERT INTO `null_template_users` (identifier, name, money, job)
+        VALUES(?, ?, ?, ?)
+        ON DUPLICATE KEY UPDATE name = ?, money = ?, job = ?
+    ]], {
+        identifier, playerName, defaultMoney, defaultJob, --INSERT値
+        playerName, defaultMoney, defaultJob -- UPDATE値
+    })
+    print(('[DATABASE] プレイヤー同期完了: %s (%s)'):format(playerName, identifier))
+end
+
 RegisterCommand('null_db_test', function (source, args)
     local xPlayer_identifier = "license:xxxxxxxx"
     local targetMoney = tonumber(args[1]) or 1000
