90116933729175f84ecb6d7765b7dc12302f8ae9
null-laptop <170277151+senganull@users.noreply.github.com>
2026-02-10 14:36:42 +0900

    fxmanifestを実装

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'senganull'
+version '1.0.0'
+
+-- サーバー側でoxmysqlの機能を使用するための宣言
+-- oxmysqlは@oxmysql/lib/MySQL.luaを読み込むことで、MySQLオブジェクトが使える
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server_sv_main.lua'
+}
+
+files {
+    'schema.sql'
+}
