a9bed9f4f3607e44c3159bb0af2fcccedbd2a336
null-laptop <170277151+senganull@users.noreply.github.com>
2026-02-10 15:17:43 +0900

    コマンドを実装

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -24,3 +24,14 @@ local function SaveUserMoney(identifier, amount)
     end
 end
 
+RegisterCommand('null_db_test', function (source, args)
+    local xPlayer_identifier = "license:xxxxxxxx"
+    local targetMoney = tonumber(args[1]) or 1000
+
+    SaveUserMoney(xPlayer_identifier, targetMoney)
+
+    local data = GetUserData(xPlayer_identifier)
+    if data then
+        print(('[DATABASE] 取得結果: 名前=%s, 所持金=%d'):format(data.name, data.money))
+    end
+end, true)
