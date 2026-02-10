e792f053f75f92df7b1cfbe0836409dcb0e57e25
null-laptop <170277151+senganull@users.noreply.github.com>
2026-02-10 15:25:35 +0900

    GetUserData関数を削除

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -1,13 +1,3 @@
---データ取得
--- MySQL.prepare.awaitは単一の値や行を取得するのに適したモダンな書き方
-local function GetUserData(identifier)
-    local result = MySQL.prepare.await('SELECT * FROM `null_template_users` WHERE `identifier` = ?', {
-        identifier
-    })
-
-    return result
-end
-
 --licenseを取得
 local function GetPlayerLicense(source)
     for _, identifier in ipairs(GetPlayerIdentifiers(source)) do
